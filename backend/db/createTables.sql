0dbdcf20ff7b91d48bc59c0d57d3e4c3808d7476
Course Test User <cs307-user@fall2024-comp307-group06.cs.mcgill.ca>
2024-11-24 15:27:19 -0500

    Restructure project into frontend and backend directories

---
+++ backend/db/createTables.sql
@@ -0,0 +1,69 @@
+-- Members Table --
+
+CREATE TABLE Member (
+    email TEXT PRIMARY KEY,
+    encryptedPassword TEXT NOT NULL,
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL
+);
+
+-- Appointments Table --
+
+CREATE TABLE Appointment ( 
+    appointmentId TEXT PRIMARY KEY, 
+    mode TEXT CHECK (mode IN ('recurring', 'one-time')), 
+    creator TEXT NOT NULL, 
+    startDate DATE NOT NULL, 
+    endDate DATE NOT NULL CHECK (endDate > startDate), 
+    topic TEXT NOT NULL, 
+    appointmentURL TEXT NOT NULL UNIQUE 
+);
+
+-- Timeslot Table --
+
+CREATE TABLE Timeslot ( 
+    timeslotID TEXT PRIMARY KEY, 
+    appointmentId TEXT NOT NULL UNIQUE, 
+    startTime TIME NOT NULL, 
+    endTime TIME NOT NULL CHECK (startTime < endTime), 
+    timeslotDate DATE NOT NULL, 
+    appointee TEXT, 
+    isRequest BOOLEAN NOT NULL, 
+    requestStatus TEXT CHECK (requestStatus IN ('approved', 'pending')),
+    
+    FOREIGN KEY (appointmentId) REFERENCES Appointment(appointmentId) 
+);
+
+-- Booking Table --
+
+CREATE TABLE Booking (
+    timeslotID TEXT NOT NULL,
+    appointeeEmail TEXT NOT NULL,
+    upcoming BOOLEAN NOT NULL,
+    
+    FOREIGN KEY (timeslotID) REFERENCES Timeslot(timeslotID)
+);
+
+-- Polling Table --
+
+CREATE TABLE Polling (
+    pollId TEXT PRIMARY KEY,
+    pollName TEXT NOT NULL,
+    pollQuestion TEXT NOT NULL,
+    creator TEXT NOT NULL,
+    isActive BOOLEAN NOT NULL,
+    pollUrl TEXT NOT NULL UNIQUE
+);
+
+-- Polling Slot Table --
+
+CREATE TABLE PollingSlot (
+    pollSlotId TEXT PRIMARY KEY,
+    pollId TEXT NOT NULL,
+    voteCount INTEGER NOT NULL,
+    startTime DATE NOT NULL,
+    endTime DATE NOT NULL CHECK (startTime < endTime),
+    pollingSlotDate DATE NOT NULL,
+
+    FOREIGN KEY (pollId) REFERENCES Polling(pollId)
+);
