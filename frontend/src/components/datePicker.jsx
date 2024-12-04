2c7a15bf7bcd0293a7528426def56f0d463da787
Chris1C <chris2012chen@gmail.com>
2024-12-04 15:27:44 -0500

    Comment out unused components

--- frontend/src/components/datePicker.jsx
+++ frontend/src/components/datePicker.jsx
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
-import {Button} from "@mui/material"
+// import React, { useState } from 'react';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import dayjs from 'dayjs';
+// import timezone from 'dayjs/plugin/timezone';
+// import utc from 'dayjs/plugin/utc';
+// import {Button} from "@mui/material"
 
-// reference : https://github.com/MitterYourTechMate/datepicker/blob/main/src/App.js
-//TODO rewrite!
-const DatePickerComponent = () => {
-  // dayjs.tz.setDefault('Pacific/Auckland');
-  let date = new Date()
-  const [value, setValue] = React.useState(dayjs(date));
+// // reference : https://github.com/MitterYourTechMate/datepicker/blob/main/src/App.js
+// //TODO rewrite!
+// const DatePickerComponent = () => {
+//   // dayjs.tz.setDefault('Pacific/Auckland');
+//   let date = new Date()
+//   const [value, setValue] = React.useState(dayjs(date));
   
-  const handleClick = () => {
-    alert(`Your Appointment is booked on ${dayjs(value).format('DD-MM-YYYY')}`)
-  }
+//   const handleClick = () => {
+//     alert(`Your Appointment is booked on ${dayjs(value).format('DD-MM-YYYY')}`)
+//   }
 
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker           
-      value={value}
-      onChange={(newValue) => {
-        setValue(newValue)
-        }}></DatePicker>
-        {/* <Button variant="contained" onClick={handleClick} style={{marginLeft:"70%"}}>BOOK APPOINTMENT</Button> */}
-      </LocalizationProvider>
-  );
-};
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//       <DatePicker           
+//       value={value}
+//       onChange={(newValue) => {
+//         setValue(newValue)
+//         }}></DatePicker>
+//         {/* <Button variant="contained" onClick={handleClick} style={{marginLeft:"70%"}}>BOOK APPOINTMENT</Button> */}
+//       </LocalizationProvider>
+//   );
+// };
 
-export default DatePickerComponent;
+// export default DatePickerComponent;
