149f772885402c08447f5ec779f8f39616bcdfd9
abrarfuad27 <abrarfuad51@gmail.com>
2024-12-08 20:45:18 -0500

    Add responsiveness to user navbar

--- frontend/src/components/navbarUser.jsx
+++ frontend/src/components/navbarUser.jsx
@@ -1,25 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../style/navbarUser.css";
 import slotifyLogo from "../assets/slotify-logo.png";
 
 const NavBarUser = () => {
+  // state to toggle the hamburger menu
+  const [toggle, setToggle] = useState(false);
+
+  // function to toggle the hamburger menu
+  const handleHamburgerToggle = () => {
+    setToggle(!toggle);
+  };
+
+  // Side effect to add/remove no-scroll class on body
+  useEffect(() => {
+    if (toggle) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [toggle]);
+
   return (
-    <nav className="navbar-user">
-      <div className="logo">
-        <Link to="/">
-          <img src={slotifyLogo} alt="Slotify Logo" className="logo" />
-        </Link>
-      </div>
-      <ul className="nav-links">
-        <li className="dropdown">
-          <Link to="/bookAppointment">Book Appointment</Link>
-        </li>
-        <li className="dropdown">
-          <Link to="/poll">Access Poll</Link>
-        </li>
-      </ul>
-      <div className="auth-buttons">
+    <>
+      <nav className="navbar-user">
+        <div className="logo">
+          <Link to="/">
+            <img src={slotifyLogo} alt="Slotify Logo" className="logo" />
+          </Link>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/bookAppointment">Book Appointment</Link>
+          </li>
+          <li>
+            <Link to="/poll">Access Poll</Link>
+          </li>
+        </ul>
+        <div className="auth-buttons">
+          <Link to="/userLogin" className="auth-button login-button">
+            Log In
+          </Link>
+          <Link to="/userRegister" className="auth-button signup-button">
+            Sign Up
+          </Link>
+        </div>
+        <div className="hamburger" onClick={handleHamburgerToggle}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </nav>
+      {/* rendered when the hamburger menu is clicked (hamburger visible when screen is below 900px) */}
+      <div className={`hamburger-content ${toggle ? "active" : ""}`}>
+        <div className="close-icon" onClick={handleHamburgerToggle}>
+          &#10005; {/* Unicode for cross icon */}
+        </div>
+        <Link to="/bookAppointment">Book Appointment</Link>
+        <Link to="/poll">Access Poll</Link>
         <Link to="/userLogin" className="auth-button login-button">
           Log In
         </Link>
@@ -27,7 +65,7 @@ const NavBarUser = () => {
           Sign Up
         </Link>
       </div>
-    </nav>
+    </>
   );
 };
 
