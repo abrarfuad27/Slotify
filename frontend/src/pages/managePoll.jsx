8bdbd721db10e6c36a3c9715cf690f8862c1b612
Chris1C <chris2012chen@gmail.com>
2024-12-13 12:25:57 -0500

    Adding appropriate message when no data to display in manage poll table

--- frontend/src/pages/managePoll.jsx
+++ frontend/src/pages/managePoll.jsx
@@ -197,11 +197,11 @@ const ManagePoll = () => {
                 <h1>Manage my polls</h1>
                 <div className='active-polls'>
                     <h2>Active poll(s)</h2>
-                    <PollViewingTable content={activePolls && activePolls.length !== 0 ? activePolls : <tr><td colSpan='3'>No polls found.</td></tr>}/>
+                    <PollViewingTable content={activePolls && activePolls.length !== 0 ? activePolls : <tr><td colSpan='3'>No polls yet.</td></tr>}/>
                 </div>
                 <div className='closed-polls'>
                     <h2>Closed poll(s)</h2>
-                    <PollViewingTable content={inactivePolls && inactivePolls.length !== 0 ? inactivePolls : <tr><td colSpan='3'>No polls found.</td></tr>}/>
+                    <PollViewingTable content={inactivePolls && inactivePolls.length !== 0 ? inactivePolls : <tr><td colSpan='3'>No polls yet.</td></tr>}/>
                 </div>
             </div>
             <Footer/>
